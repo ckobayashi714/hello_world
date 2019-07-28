4549e7609ea138b7afb4bbee46cb533324021b7d
ckobayashi714 <ckobayashi@csu.fullerton.edu>
2019-07-28 00:26:01 -0700

    homework SSH & Git

--- hello_world.c
+++ hello_world.c
@@ -3,6 +3,6 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]){
-	puts("Hello world!");
+	puts("Hello Professor Shafae!");
 	return 0;
 }
---
+++ hello_world.d
@@ -0,0 +1,1 @@
+hello_world.o hello_world.d : hello_world.c
